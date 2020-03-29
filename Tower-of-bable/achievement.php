98fac32959e8f8166f830f05366bb411274b0896
HodKray <48974326+HodKray@users.noreply.github.com>
2020-03-29 23:56:18 +0700

    Add files via upload

---
+++ Tower-of-bable/achievement.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Достижения</title>
+	<?php
+		include 'head.php';
+	?>
+</head>
+<body>
+
+<?php
+	include 'header.php';
+?>
+
+<h1 style="text-align: center;">Достижения</h1>
+
+<?php
+	include 'footer.php';
+?>
+
+</body>
+</html>
---
+++ Tower-of-bable/header.php
@@ -0,0 +1,50 @@
+<style>
+  .header {
+    background: #8d705a;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+    border-radius: 0px 0px 10px 10px;
+  }
+  .navbar-light .navbar-brand {
+    color: rgba(255, 255, 255, 1);
+  }
+  .navbar-light .navbar-nav .nav-link {
+    color: rgba(255, 255, 255, 1);
+  }
+</style>
+
+<header class="mb-5 header sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bd-highlight">
+  <div class="container">
+   <a class="navbar-brand" href="/">
+    <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Tower of babel
+  </a>
+  <button class="navbar-toggler" type="button" data-target="#my-nav" aria-expanded="false" aria-label="Toggle navigation" data-toggle="collapse">
+        <span class="navbar-toggler-icon"></span>
+  </button>
+  <div id="my-nav" class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/"><img src="img/nav-icon/home.png" width="12" class="d-inline-block" alt="">
+        Главная <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="rating.php"><img src="img/nav-icon/rating.png" width="14" class="d-inline-block" alt="">
+        Рейтинг</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="mygame.php"><img src="img/nav-icon/mygame.png" width="14" class="d-inline-block" alt="">
+        Мои игры</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="achievement.php"><img src="img/nav-icon/achievement.png" width="10" class="d-inline-block" alt="">
+        Мои достижения</a>
+      </li>
+    </ul>
+    <a class="navbar-brand" href="profile.php">Мой профиль
+      <img src="img/profile-icon-white.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    </a>
+  </div>
+  </div>
+</nav>
+</header>
