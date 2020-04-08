46007ab1b6b4d9f51e1b66e15e84e0f076b3d570
Sharpmax <max.potudinskiy@mail.ru >
2020-04-08 20:17:34 +0700

    Fix auth

--- auth/action_register.php
+++ auth/action_register.php
@@ -27,7 +27,7 @@
 
 		setcookie('user', $user[0]['nickname'], time() + 3600, "/");
 
-		header("Location: /register.php");
+		header("Location: ../register.php");
 	} else {
 		echo 'Пароли не совпадают!';
 		die();
